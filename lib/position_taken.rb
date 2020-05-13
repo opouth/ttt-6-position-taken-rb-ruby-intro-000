0f6dc0adc716625ba27a5cc851ba32e0f89de354
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 02:25:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  if board[index] = [] 
+  if board[index] = []
     return false
   else board[index] == "X" || board[index] == "O"
     return true
