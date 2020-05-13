b500e96a0552cf0870743f3e6563263b6d62b145
Ermite Rikudo <opouth@hotmail.fr>
2020-05-13 02:09:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,8 @@
 # code your #position_taken? method here!
 #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 #index = 0
-board = []
-index = 0
 
-def position_taken(board, index)
+
+def position_taken?(board, index)
 
 end
